58f061c56b3ebf22d823052a67d4b4c8749076ab
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-05-26 18:39:13 -0700

    Complete the first phase of ex1

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,11 +17,12 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    newTheta = theta;
+    % newTheta(1) = theta(1) - alpha * (1/m) * sum(((theta' * X') - y')' .* X(:,1)')
+    % newTheta(2) = theta(2) - alpha * (1/m) * sum((X*theta) - y)')
+    newTheta(1) = theta(1) - alpha * (1/m) * sum(((X*theta) - y)' .* (X(:,1)'));
+    newTheta(2) = theta(2) - alpha * (1/m) * sum(((X*theta) - y)' .* (X(:,2)'));
+    theta = newTheta;
 
     % ============================================================
 
