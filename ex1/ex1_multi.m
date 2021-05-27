6e3a5f9acf851f1daa31ec805728c52e814c32ab
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-05-27 12:56:32 -0700

    Complete ex1

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 1;
+num_iters = 50;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -105,8 +105,8 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 test_X = [1650 3];
-test_norm = (test_X - mu) ./ sigma
-test_norm = [1 test_norm]
+test_norm = (test_X - mu) ./ sigma;
+test_norm = [1 test_norm];
 price = test_norm * theta;
 
 % ============================================================
@@ -151,8 +151,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0;
 
+price = [1 1650 3] * theta;
 
 % ============================================================
 
